7f07e7b139561e63d95190f7576535b205576800
rotemtulila <rotem.tulila@ex.co>
2021-12-16 18:53:30 +0200

    adding full project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import React,{ useState} from 'react';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    const  [usersList,setUsersList] =useState([]);
+    const addUserHandler = (uName,uAge)=>{
+      setUsersList((prev)=>{
+          return [...prev,{name:uName,age:uAge, id:Math.random().toString()}]
+      });
+    };
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler}/>
+            <UsersList users={usersList}/>
+        </div>
+);
 }
 
 export default App;
